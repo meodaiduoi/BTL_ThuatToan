01348854ea3225c2e180c8a30f84617b09174c07
Duong Cong Son <62704959+CongSon01@users.noreply.github.com>
2021-07-17 18:58:14 +0700

    add select type of array

--- src/RandomArray.java
+++ src/RandomArray.java
@@ -5,32 +5,59 @@ public class RandomArray {
     private int size = 10;
     private ArrayList<Number> array = new ArrayList<>();
     RandomArray () {
-        arrayGenerator(true);
+        arrayGenerator(0);
     }
 
     RandomArray (int size) {
         this.size = size;
-        arrayGenerator(true);
+        arrayGenerator(0);
     }
 
-    RandomArray (int size, boolean isInt) {
+    RandomArray (int size, int type_array) {
         this.size = size;
-        arrayGenerator(isInt);
+        arrayGenerator(type_array);
     }
 
-    private void arrayGenerator (boolean isInt) {
-        if (isInt) {
-            Random rd = new Random();
-            for (int i = 0; i < this.size; i++) {
-                // this.array.add(rd.nextInt((100 + 100) + 1) - 100);
-                this.array.add(rd.nextInt());
-            }
-        }
-        else {
-            Random rd = new Random();
-            for (int i = 0; i < this.size; i++) {
-                this.array.add(rd.nextDouble());
-            }
+    private void arrayGenerator (int type_array) {
+        Random rd = new Random();
+        switch(type_array) {
+            case 0:
+                // random array
+                for (int i = 0; i < this.size; i++) {
+                    this.array.add(rd.nextInt((100 + 100) + 1) - 100);
+                    // this.array.add(rd.nextInt());
+                }
+                break;
+            case 1:
+                // sorted array
+                for (int i = 0; i < this.size; i++) {
+                    this.array.add(i);
+                }
+                break;
+            case 2:
+                // inverse array
+                for (int i = this.size; i > 0; i--) {
+                    this.array.add(i);
+                }
+                break;
+            case 3:
+                // Mảng sai 1 vài phần tử
+                for (int i = 0; i < size; i++) {
+                    if(i == (size-1)) {
+                        Number tmp = array.get(i-1);
+                        array.set(i-1, array.get(i - 2));
+                        array.set(i - 2, tmp);
+                    }
+                    array.add(i);
+                }
+                break;
+            default:
+              // code block
+              // random array
+                for (int i = 0; i < this.size; i++) {
+                    // this.array.add(rd.nextInt((100 + 100) + 1) - 100);
+                    this.array.add(rd.nextInt());
+                }
         }
     }
 
