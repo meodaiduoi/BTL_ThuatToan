7095af3abbba871e87e41d042e48c21b6ffd87e5
meodaiduoi <28221040+meodaiduoi@users.noreply.github.com>
2021-08-06 08:44:16 +0700

    New ui

--- app/src/main/java/sort/Controller.java
+++ app/src/main/java/sort/Controller.java
@@ -7,7 +7,6 @@ import javafx.scene.chart.XYChart;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
@@ -21,6 +20,7 @@ import javafx.scene.image.Image;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 public class Controller implements Initializable {
 
     public Controller() {}
